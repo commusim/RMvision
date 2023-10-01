fe548cc9441bb6fded0d11ec92f83a64db19565f
commusim <2225229189@qq.com>
2023-10-01 21:48:29 +0800

    bin

---
+++ build/src/CMakeFiles/slovepose.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../bin/slovepose"
+  "../../bin/slovepose.pdb"
+  "CMakeFiles/slovepose.dir/PNP.cpp.o"
+  "CMakeFiles/slovepose.dir/PNP.cpp.o.d"
+  "CMakeFiles/slovepose.dir/real3D.cpp.o"
+  "CMakeFiles/slovepose.dir/real3D.cpp.o.d"
+  "CMakeFiles/slovepose.dir/recognition.cpp.o"
+  "CMakeFiles/slovepose.dir/recognition.cpp.o.d"
+  "CMakeFiles/slovepose.dir/slovepose.cpp.o"
+  "CMakeFiles/slovepose.dir/slovepose.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slovepose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/slovepose.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for slovepose.
+# This may be replaced when dependencies are built.
